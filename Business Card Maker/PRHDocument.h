832fae448be136128ebaa3b0b1d8f3b5a30876db
Peter Hosey <git@boredzo.org>
2012-09-12 01:41:48 -0700

    Initial Commit

---
+++ Business Card Maker/PRHDocument.h
@@ -0,0 +1,13 @@
+//
+//  PRHDocument.h
+//  Business Card Maker
+//
+//  Created by Peter Hosey on 2012-09-12.
+//  Copyright (c) 2012 Peter Hosey. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface PRHDocument : NSDocument
+
+@end
---
+++ Business Card Maker/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  Business Card Maker
+//
+//  Created by Peter Hosey on 2012-09-12.
+//  Copyright (c) 2012 Peter Hosey. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+	return NSApplicationMain(argc, (const char **)argv);
+}
